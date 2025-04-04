738896a7ed60dba5a7a975863df61802a4ef747e
PAOLA FLORES GUERRERO <160794901+llSRRERO9@users.noreply.github.com>
2025-04-03 18:13:43 -0600

    Create print_n_numbers.py

---
+++ .github/workflows/python/print_n_numbers.py
@@ -0,0 +1,9 @@
+try:
+    n=10
+    print("El numero seleccionado es:",n)
+
+    for x in range(n):
+    print(x)
+
+except ValueError:
+  print("Error: No se puede convertir el parametro proporcionado a entero")
